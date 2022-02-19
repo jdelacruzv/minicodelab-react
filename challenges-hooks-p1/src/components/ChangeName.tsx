474d9896e4a4576bbdf5171af4136f7e329d32c4
José De La Cruz <44282475+jdelacruzv@users.noreply.github.com>
2022-02-19 03:13:59 -0500

    Fixed file tab

--- challenges-hooks-p1/src/components/ChangeName.tsx
+++ challenges-hooks-p1/src/components/ChangeName.tsx
@@ -1,27 +1,27 @@
 import { useState, useEffect } from 'react';
 
 export const ChangeName: React.FC = () => {
-	const [name, setName] = useState<string>('MiniCodeLab');
+  const [name, setName] = useState<string>('MiniCodeLab');
   const [newName, setNewName] = useState<string>('');
 
   useEffect(() => {
-		setName(newName.toUpperCase());
-	}, [newName]);
+    setName(newName.toUpperCase());
+  }, [newName]);
 
   useEffect(() => {
     setName("Learning React");
   }, []);
 
   return (
-		<>
-			<h2>Reto 03 - Cambia el nombre en el Render</h2>
-			<h3>El nombre es: {name}</h3>
-			<input
-				type="text"
-				value={name}
-				onChange={(e) => setNewName(e.target.value)}
-			/>
-		</>
-	);
+    <>
+      <h2>Reto 03 - Cambia el nombre en el Render</h2>
+      <h3>El nombre es: {name}</h3>
+      <input
+	type="text"
+	value={name}
+	onChange={(e) => setNewName(e.target.value)}
+      />
+    </>
+  );
 };
 
