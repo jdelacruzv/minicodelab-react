b6ec7277f251e10c326304f215da0ea6f0f73da6
Jose De La Cruz <jldlcv@gmail.com>
2022-02-21 08:53:17 -0500

    Fixed tabulation

--- challenges-hooks-p1/src/components/ChangeName.tsx
+++ challenges-hooks-p1/src/components/ChangeName.tsx
@@ -1,35 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const ChangeName: React.FC = () => {
-  const [name, setName] = useState<string>('MiniCodeLab');
-  const [isInputUsed, setIsInputUsed] = useState<boolean>(false);
+	const [name, setName] = useState<string>("MiniCodeLab");
+	const [isInputUsed, setIsInputUsed] = useState<boolean>(false);
 
-  useEffect(() => {
-    // Cambia el state al montarse el componente a Learning React
-    setName("Learning React");
+	useEffect(() => {
+		// Cambia el state al montarse el componente a Learning React
+		setName("Learning React");
 	}, []);
 
-  useEffect(() => {
-    if (isInputUsed) {
-      setName(name.toUpperCase());
-    }
-  }, [name, isInputUsed]);
+	useEffect(() => {
+		if (isInputUsed) {
+			setName(name.toUpperCase());
+		}
+	}, [name, isInputUsed]);
 
-  return (
-    <>
-      <h2>Reto 03 - Cambia el nombre en el Render</h2>
-      <h3>El nombre es: {name}</h3>
-      <input 
-        type="text" 
-        value={name} 
-        onChange={(e) => {
-          setName(e.target.value)
-          if (!isInputUsed) {
-            setIsInputUsed(true)}
-          }
-        }
-      />
-    </>
-  );
+	return (
+		<>
+			<h2>Reto 03 - Cambia el nombre en el Render</h2>
+			<h3>El nombre es: {name}</h3>
+			<input
+				type="text"
+				value={name}
+				onChange={(e) => {
+					setName(e.target.value);
+					if (!isInputUsed) {
+						setIsInputUsed(true);
+					}
+				}}
+			/>
+		</>
+	);
 };
-
--- challenges-hooks-p1/src/components/RickAndMortyFilter.tsx
+++ challenges-hooks-p1/src/components/RickAndMortyFilter.tsx
@@ -1,43 +1,46 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 type RickAndMorty = {
-  id: number;
-  name: string;
-  image: string;
-  status: string;
+	id: number;
+	name: string;
+	image: string;
+	status: string;
 };
 
 export const RickAndMortyFilter: React.FC = () => {
-  const [characterList, setCharacterList] = useState<RickAndMorty[]>([]);
-  const [filter, setFilter] = useState<string>('');
+	const [characterList, setCharacterList] = useState<RickAndMorty[]>([]);
+	const [filter, setFilter] = useState<string>("");
 
-  useEffect(() => {
-    // función autoinvocada
-    (async () => {
-      let data = await fetch(`https://rickandmortyapi.com/api/character/`).then((res) => res.json());
-      setCharacterList(data.results);
-    })();
-  }, []);
-  
-  const filteredCharacter = characterList.filter((character) => 
-    character.name.toLowerCase() === filter.trim().toLowerCase());
+	useEffect(() => {
+		// función autoinvocada
+		(async () => {
+			let data = await fetch(`https://rickandmortyapi.com/api/character/`).then(
+				(res) => res.json()
+			);
+			setCharacterList(data.results);
+		})();
+	}, []);
 
-  useEffect(() => {
+	const filteredCharacter = characterList.filter(
+		(character) => character.name.toLowerCase() === filter.trim().toLowerCase()
+	);
+
+	useEffect(() => {
 		filteredCharacter.map((character) => setFilter(character.name));
 	});
 
-  return (
-    <>
+	return (
+		<>
 			<h2>Reto 05 - Filtrado de elementos</h2>
-      <h3>'Rick And Morty'</h3>
-			<div className='character-container'>
+			<h3>'Rick And Morty'</h3>
+			<div className="character-container">
 				<ul className="character-list">
-          {characterList.map((character) => (
-            <li key={character.id} className="character-list">
+					{characterList.map((character) => (
+						<li key={character.id} className="character-list">
 							{character.name}
 						</li>
 					))}
-				</ul>				
+				</ul>
 				<div className="character-input">
 					<input
 						type="text"
@@ -47,13 +50,13 @@ export const RickAndMortyFilter: React.FC = () => {
 					/>
 					<h3>{filter}</h3>
 					{filteredCharacter.map((character) => (
-            <div key={character.id} >
-              <img src={character.image} alt={character.name} />
-              <p>{character.status}</p>
-            </div>
+						<div key={character.id}>
+							<img src={character.image} alt={character.name} />
+							<p>{character.status}</p>
+						</div>
 					))}
 				</div>
 			</div>
 		</>
-  );
-}
+	);
+};
